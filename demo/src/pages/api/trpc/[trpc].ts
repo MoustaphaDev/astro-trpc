8da766891dcd516fd0e04adc9d2a842cbc310470
Moustapha HappyDev <moustapha.happydev@gmail.com>
2022-09-18 21:52:51 +0000

    fix: ignored type declarations on build
    * modified imports on demo to match the new type paths
    
    * bumped package version

--- demo/src/pages/api/trpc/[trpc].ts
+++ demo/src/pages/api/trpc/[trpc].ts
@@ -1,5 +1,4 @@
-import { createAstroTRPCApiHandler } from 'astro-trpc/dist/server';
-
+import { createAstroTRPCApiHandler } from 'astro-trpc/server';
 import * as trpc from '@trpc/server';
 import { z } from 'zod';
 export const appRouter = trpc.router().query('greeting', {
